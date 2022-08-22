0e1b4a5dcf5bd326f9701f032b571fee3ef8b5ec
Nastya125 <nastyatrue@mail.ru>
2022-08-22 21:08:39 +0300

    3 задание

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,33 @@
+const hole = index => document.getElementById(`hole${index}`);
+let winCounter = 0;
+let loseCounter = 0;
+
+    for (let i = 1; i < 10; i++) {
+        hole(i).onclick = function(){
+
+            if(this.className.includes("hole hole_has-mole")) { 
+                winCounter +=1;
+                dead.textContent = `${winCounter}`
+            } else {
+                loseCounter +=1;
+                lost.textContent = `${loseCounter}`
+            }
+            console.log(loseCounter)
+
+            checkWin(winCounter, loseCounter);
+        }  
+    }
+    
+    function checkWin(winMole, loseMole){
+        
+        if(winMole === 10){
+            alert("Вы победили");
+            location.reload();
+        }
+
+        if(loseMole === 5){
+            alert("Вы проиграли!");
+            location.reload();
+        }
+    
+    }
