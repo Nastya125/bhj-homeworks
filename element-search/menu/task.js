70d8d915f457f63280403e78b16bad64d96ae905
Nastya125 <nastyatrue@mail.ru>
2022-08-24 21:39:01 +0300

    Task 2 Навигационное меню

---
+++ element-search/menu/task.js
@@ -0,0 +1,30 @@
+
+const menuLinks = document.querySelectorAll("a.menu__link");
+const arrMenuLinks = Array.from(menuLinks);
+let count = 0;
+
+
+
+for (let i = 0; i < arrMenuLinks.length; i++){
+    arrMenuLinks[i].onclick = function() {
+        console.log("Клик");
+   
+    let menuWithSubMenu = arrMenuLinks[i].closest("li.menu__item").querySelector('ul.menu_sub');
+    
+
+        if(menuWithSubMenu !== null && count === 0){
+            menuWithSubMenu.className += ' menu_active';
+            count = 1;
+            return false;
+        }
+
+        if(menuWithSubMenu !== null && count === 1){
+            menuWithSubMenu.className = targetItem.className.replace(' menu_active', '');
+            count = 0;
+            return false
+        }
+
+
+    }
+}
+
