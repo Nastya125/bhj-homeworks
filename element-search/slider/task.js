f898352b8ea9ec42dd3ec3be76f5994bf2653a35
Nastya125 <nastyatrue@mail.ru>
2022-08-28 21:50:00 +0300

    Последние правки

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,61 +5,18 @@ const arrSliders = Array.from(document.querySelectorAll("div.slider__item"))
 const dots = Array.from(document.querySelectorAll("div.slider__dot"));
 
 
-
-for(let dot in dots){
-  
- dots[0].className = "slider__dot slider__dot_active";
-    
-
-  dots[dot].onclick = function(){
-        let active = arrSliders.findIndex(slider => slider.className === "slider__item slider__item_active");
-        let activeDot = dots.findIndex(dot => dot.className === "slider__dot slider__dot_active");
-        
-        active = dot;
-
-        arrSliders[activeDot].className = "slider__item"; 
-        dots[activeDot].className = "slider__dot";
-
-        arrSliders[active].className = "slider__item slider__item_active";
-        dots[active].className = "slider__dot slider__dot_active";
-
-    } 
-
-
-
-
-rightSliderArrows.onclick = function(){
+function getSliders(position){
     let active = arrSliders.findIndex(slider => slider.className === "slider__item slider__item_active");
-    let activeDot = dots.findIndex(dot => dot.className === "slider__dot slider__dot_active");
+    arrSliders[active].className = "slider__item"; 
+    dots[active].className = "slider__dot";
+   
+    active = position;
 
-    arrSliders[activeDot].className = "slider__item"; 
-    dots[activeDot].className = "slider__dot";
-
-
-    active += 1;
-
-    if (active === arrSliders.length) {
+    if (active > arrSliders.length - 1) {
         active = 0;
     }
 
-    arrSliders[active].className = "slider__item slider__item_active";
-    dots[active].className = "slider__dot slider__dot_active";
-
-
-}
-
-
-
-leftSliderArrows.onclick = function(){
-    let active = arrSliders.findIndex(slider => slider.className === "slider__item slider__item_active");
-    let activeDot = dots.findIndex(dot => dot.className === "slider__dot slider__dot_active");
-
-    arrSliders[activeDot].className = "slider__item"; 
-    dots[activeDot].className = "slider__dot";
-
-    active -= 1;
-
-    if (active === - 1) {
+    if (active < 0) {
         active = arrSliders.length - 1;
     }
 
@@ -68,8 +25,26 @@ leftSliderArrows.onclick = function(){
 
 }
 
-}
 
+for(let dot in dots){
+  
+    dots[0].className = "slider__dot slider__dot_active";
+
+    dots[dot].onclick = function(){
+        getSliders(dot);
+    } 
+
+    rightSliderArrows.onclick = function(){
+        let active = arrSliders.findIndex(slider => slider.className === "slider__item slider__item_active");
+        getSliders(active+1);
+    }
+
+    leftSliderArrows.onclick = function(){
+        let active = arrSliders.findIndex(slider => slider.className === "slider__item slider__item_active");
+        getSliders(active-1);
+    }
+
+}
 
 
     
