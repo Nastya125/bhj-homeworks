4d69bb976da16b4d107add452e27c444f84ebf5c
Nastya125 <nastyatrue@mail.ru>
2022-09-21 22:06:05 +0300

    task 3

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,9 +1,32 @@
-const button = document.getElementById('signin__btn');
+const form = document.getElementById('signin__form');
 
-button.addEventListener('click', ()=> {
-    const form = document.getElementById('signin');
+const welcome = document.getElementById('welcome');
+const id = document.getElementById('user_id');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+   
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-
+    xhr.onload = function() {
+        if (xhr.status != 200) { 
+            alert(`Ошибка`); 
+          } else { 
+            let data = JSON.parse(this.responseText);
+            localStorage.setItem('data', data);
+            
+            if(data.success === false){
+                alert('Неверный логин/пароль')
+            } else {
+                welcome.classList.add('welcome_active')
+                id.innerHTML = data.user_id;
+            }
+          }
+    }
+    xhr.send(formData);
 })
+
+
+
