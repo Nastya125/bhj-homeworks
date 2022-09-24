5ddd5af1d286c08c55e6b6f794478ae065862c44
Nastya125 <nastyatrue@mail.ru>
2022-09-24 11:56:31 +0300

    Небольшие правки

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -9,13 +9,12 @@ form.addEventListener('submit', (e) => {
    
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    xhr.onload = function() {
-        if (xhr.status != 200) { 
-            alert(`Ошибка`); 
-          } else { 
-            let data = JSON.parse(this.responseText);
+    xhr.onload = function() { 
+            let data = xhr.response;
             localStorage.setItem('data', data);
+            console.log(data)
             
             if(data.success === false){
                 alert('Неверный логин/пароль')
@@ -23,7 +22,7 @@ form.addEventListener('submit', (e) => {
                 welcome.classList.add('welcome_active')
                 id.innerHTML = data.user_id;
             }
-          }
+          
     }
     xhr.send(formData);
 })
